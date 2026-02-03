25a3ed9570037d24c528ed4f7575aed35b4dcded
Rajat7387 <rajat.billore@gmail.com>
2026-02-03 07:23:33 +0530

    new changes

--- Python_series/Day_3_Input_TypeCasting_And_Basic_Calculation.py
+++ Python_series/Day_3_Input_TypeCasting_And_Basic_Calculation.py
@@ -39,8 +39,58 @@
 # print(total_sales)
 # print(type(total_sales))
 
+# exampele 1:
+# product = input("Enter the product name: ")
+# quantity = input("Enter the quantity: ")
+# price_per_item = input("Enter the price per item: ")
+
+# total_sales = int(quantity) * float(price_per_item)
+
+# print ("Product:", product)
+# print("Total sales amount is:", total_sales)    
 
 
+# example 2:
+
+# num1 = 20 
+# num2 = 30
+# sum = num1 + num2
+# print(sum)
+
+
+# Assignment 1:
+# make a Salary slip Calculation :
+
+# employee_name = input("Enter the employee name: ")
+# basic_salary = float(input("Enter the basic salary: "))
+# bonus_amount = float(input("Enter the bonus amount: "))
+# tax_percentage = float(input("Enter the tax percentage: "))
+
+# # Ccalculate 
+# gross_salary = basic_salary + bonus_amount
+# tax_amount = (tax_percentage / 100) * gross_salary
+# net_salary = gross_salary - tax_amount
+# print("Employee Name:", employee_name)
+# print("Gross Salary:", gross_salary)
+# print("Tax Amount:", tax_amount)
+# print("Net Salary:", net_salary)
+
+
+# Assignment 2:
+# item = input("Enter the item name: ")
+# price = float(input("Enter the price of the item: "))
+# discount_percentage = float(input("Enter the discount percentage: "))
+
+# # Calculate
+# discount_amount = (discount_percentage / 100) * price
+# final_price = price - discount_amount
+
+# print("Item Name:", item)
+# print("Original Price:", price)
+# print("Discount Amount:", discount_amount)
+# print("Final Price:", final_price)
+
+# print("Final Price of", item, "after", discount_percentage, "% discount is:", final_price)
 
 
 
